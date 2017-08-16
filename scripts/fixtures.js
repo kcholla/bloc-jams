413af40df1d1b5aeeac0ab33c17f0cf1d869d633
Kristyn Holland <holland.kristyn@gmail.com>
2017-08-15 21:32:31 -0500

    Checkpoint 31 completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21' },
+         { title: 'Magenta', duration: '2:15' }
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+
+//Third Album
+var albumThird = {
+    title: '123',
+     artist: 'The Third Guy',
+     label: 'Tre',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/15.png',
+     songs: [
+         { title: 'Good Song', duration: '1:01' },
+         { title: 'Bad Song', duration: '5:01' },
+         { title: 'This is OK', duration: '3:21'},
+         { title: 'Why Bother?', duration: '3:14' },
+         { title: 'Better Luck Next Time', duration: '2:15'}
+     ]
+ };
